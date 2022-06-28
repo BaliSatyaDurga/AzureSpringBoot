8b822fce32badeaef7cd85511fdb8d4838b6a6a9
BHANU <barlabhanusri2016.gmail.com>
2022-06-28 17:20:00 +0530

    firstCommit

---
+++ AzureSpringBoot/src/main/java/com/example/AzureSpringBoot/AppController.java
@@ -0,0 +1,15 @@
+package com.example.AzureSpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hello from azure DeOps";
+	}
+
+
+}
